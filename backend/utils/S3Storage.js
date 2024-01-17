c822bc41cd57eb210af3ee0b4b2fba744da75a95
FilipeMiranda22 <filipe.salomao@academico.ifpb.edu.br>
2024-01-17 13:18:13 -0300

    node-fetch

--- backend/utils/S3Storage.js
+++ backend/utils/S3Storage.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const aws = require("aws-sdk");
 const mime = require("mime");
+const fetch = require("node-fetch");
 
 class S3Storage {
   constructor() {
@@ -15,31 +16,36 @@ class S3Storage {
       fieldname === "image" ? "photos" : "users"
     }/${filename}`;
 
-    const response = await fetch(imageUrl);
+    try {
+      const response = await fetch(imageUrl);
 
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch image from ${imageUrl}. Status: ${response.status}`
-      );
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch image from ${imageUrl}. Status: ${response.status}`
+        );
+      }
+
+      const fileContent = await response.arrayBuffer();
+
+      const ContentType = mime.getType(filename);
+
+      if (!ContentType) {
+        throw new Error("File not found");
+      }
+
+      this.client
+        .putObject({
+          Bucket: "reactgramimg",
+          Key: `${fieldname === "image" ? "photos" : "users"}/${filename}`,
+          ACL: "public-read",
+          Body: Buffer.from(fileContent),
+          ContentType,
+        })
+        .promise();
+    } catch (error) {
+      console.error("Error saving file:", error);
+      throw error; // Rejeita a promessa com o erro original
     }
-
-    const fileContent = await response.arrayBuffer();
-
-    const ContentType = mime.getType(filename);
-
-    if (!ContentType) {
-      throw new Error("File not found");
-    }
-
-    this.client
-      .putObject({
-        Bucket: "reactgramimg",
-        Key: `${fieldname === "image" ? "photos" : "users"}/${filename}`,
-        ACL: "public-read",
-        Body: Buffer.from(fileContent),
-        ContentType,
-      })
-      .promise();
   }
 
   async deleteFile(filename) {
